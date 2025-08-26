b3bfa3215e6baf7ef2a5b834eb6ff0e8360410f4
Jeyvin Coleman <jcoleman24@huskers.unl.edu>
2025-08-26 12:06:16 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jeyvin Coleman
+ * Date: 2025-8-26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("My name is Jeyvin!\n");
+    printf("My major is Computer Engineering\n");
+
+  return 0;
+}
